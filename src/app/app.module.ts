7468892cc202650ce4d3dccd3e6891dc4fd842cd
Seyth382 <nicoelias1016@gmail.com>
2024-10-26 01:26:39 -0300

    update modules.ts (pura fe en cristo)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { Plugins } from '@capacitor/core';
-import { Storage } from '@capacitor/storage';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 
+import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireModule} from '@angular/fire/compat';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule, AngularFireAuthModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"vargasyanez-2024","appId":"1:486413180619:web:b9bbbf218d1405b0c5054e","storageBucket":"vargasyanez-2024.appspot.com","apiKey":"AIzaSyBlMq0oh1vjxsK9umDfeqtThAa6sS0zSNk","authDomain":"vargasyanez-2024.firebaseapp.com","messagingSenderId":"486413180619"})), provideAuth(() => getAuth())],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializar Firebase
+    AngularFireAuthModule, // Módulo de autenticación de Firebase
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
